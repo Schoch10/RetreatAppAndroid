194d5aec07ee884c47349bc88238b2efd889b29b
Alexander Peletz <apeletz512@gmail.com>
2015-07-06 11:25:04 -0400

    created splash screen and tested logic to load splash screen on first run only

--- app/src/main/java/slalom/com/retreatapplication/HomeActivity.java
+++ app/src/main/java/slalom/com/retreatapplication/HomeActivity.java
@@ -1,20 +1,38 @@
 package slalom.com.retreatapplication;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.content.Intent;
+import android.util.Log;
 
 
 public class HomeActivity extends ActionBarActivity {
 
+    private static final String TAG = HomeActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        Log.d(TAG, prefs.toString());
+        if (prefs.getBoolean("firstrun", true)) {
+            Log.d(TAG, "firstrun: true");
+            setContentView(R.layout.activity_home);
+            prefs.edit().putBoolean("firstrun", false).commit();
+        } else if (!prefs.getBoolean("firstrun", false)) {
+            Log.d(TAG, "firstrun: false");
+            setContentView(R.layout.activity_retreat_app_main_view);
+            prefs.edit().putBoolean("firstrun", true).commit();
+        }
+
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
