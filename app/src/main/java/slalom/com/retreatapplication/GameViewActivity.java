723ab6f3ac25232db96847e516192d74c0539efd
Jill Burgess <jill.burgess@slalom.com>
2015-08-12 00:17:24 -0400

    game layout view and sample questions implemented; answers not stored locally yet

--- app/src/main/java/slalom/com/retreatapplication/GameViewActivity.java
+++ app/src/main/java/slalom/com/retreatapplication/GameViewActivity.java
@@ -1,9 +1,16 @@
 package slalom.com.retreatapplication;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
 
 
 public class GameViewActivity extends ActionBarActivity {
@@ -12,6 +19,19 @@ public class GameViewActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_view);
+
+//        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new GridViewAdapter(this));
+
+//        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            public void onItemClick(AdapterView<?> parent, View v,
+//                                    int position, long id) {
+//                Toast.makeText(GameViewActivity.this, "" + position,
+//                        Toast.LENGTH_SHORT).show();
+//            }
+//        });
     }
 
     @Override
@@ -34,5 +54,52 @@ public class GameViewActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public class GridViewAdapter extends BaseAdapter {
+        private Context mContext;
+
+        public GridViewAdapter(Context c) {
+            mContext = c;
+        }
+
+        public int getCount() {
+            return questionsArray.length;
+        }
+
+        public Object getItem(int position) {
+            return questionsArray[position];
+        }
+
+        public long getItemId(int position) {
+            return position;
+        }
+
+        // create a new ImageView for each item referenced by the Adapter
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                LayoutInflater inflater = (LayoutInflater) mContext.getApplicationContext()
+                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(R.layout.card_view, parent, false);
+            }
+            TextView textView = (TextView)convertView.findViewById(R.id.question);
+            textView.setText(questionsArray[position]);
+            return convertView;
+        }
+
+        private String[] questionsArray = {
+                "How old is Russell?",
+                "What object does Mikey B look like when he stands?",
+                "Who Broke the Elevator?",
+                "Name an IM&A SSIS Architect?",
+                "How does Todd Richman get to work in the summer?",
+                "Who are Slalom's founders?",
+                "What is a fun fact about someone else?",
+                "What is Todd Christy's favorite snack?",
+                "What consultant was mounted by a cow?",
+                "What is the Dude's favorite hobby?",
+                "What is a fun fact about Barry?",
+                "Who has more than 3 siblings?"
+        };
     }
 }
