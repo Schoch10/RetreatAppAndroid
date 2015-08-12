51b29dbd03c15ea75646bdf2b5ed2f3d3635b876
senthilrajav <senthilrajav@slalom.com>
2015-08-12 00:18:07 -0400

    Added trending activity related files

--- app/src/main/java/slalom/com/retreatapplication/TrendingActivity.java
+++ app/src/main/java/slalom/com/retreatapplication/TrendingActivity.java
@@ -1,23 +1,69 @@
 package slalom.com.retreatapplication;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v4.widget.SwipeRefreshLayout.OnRefreshListener;
+
+import java.util.HashMap;
+
+import slalom.com.retreatapplication.db.TPartyDBHelper;
+import slalom.com.retreatapplication.util.HashMapAdapter;
+import slalom.com.retreatapplication.util.TPartyTask;
 
 
-public class TrendingActivity extends ActionBarActivity {
+public class TrendingActivity extends Activity {
+    SwipeRefreshLayout swipeLayout;
+    //Activity activityContext;
+    TPartyDBHelper dbHelper;
+    TextView textDetail;
+
+    private Integer[] imgId = {
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trending);
+        //this.activityContext = this;
+
+        swipeLayout = (SwipeRefreshLayout)findViewById(R.id.swipe_container);
+        swipeLayout.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Trigger Async Task (onPreExecute method)
+                new TPartyTask().execute("getCheckIns", this);
+                swipeLayout.setRefreshing(false);
+            }
+        });
+        swipeLayout.setColorScheme(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+        dbHelper = new TPartyDBHelper(this);
+        HashMap<String, Integer> checkIns = (HashMap<String, Integer>)dbHelper.getCheckIns();
+
+        HashMapAdapter hashMapAdapter = new HashMapAdapter(this, checkIns);
+        ListView listView = (ListView) findViewById(R.id.listView1);
+        listView.setAdapter(hashMapAdapter);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_trending, menu);
+        getMenuInflater().inflate(R.menu.menu_agenda, menu);
         return true;
     }
 
